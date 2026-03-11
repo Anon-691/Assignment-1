6007151f1f6fe210b90a7703e1163fd3a83910ef
Anon-691 <a.adeel2027@gmail.com>
2026-03-11 21:38:35 +0500

    Create FunctionOfFactorial.java

---
+++ FunctionOfFactorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class FunctionOfFactorial {
+  public static int factorial(int n){
+    int f=1;
+    for(int i=1;i<=n;i++){
+      f*=i;
+    }
+    return(f);
+  }
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number:");
+    int n=sc.nextInt();
+    int res;
+    res=factorial(n);
+    System.out.println(res);
+  }
+}
