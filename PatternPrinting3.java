fdf64b5898113f335260c2ada1ef74aa9ebb00b0
Anon-691 <a.adeel2027@gmail.com>
2026-03-11 22:40:57 +0500

    Create PatternPrinting3.java

---
+++ PatternPrinting3.java
@@ -0,0 +1,30 @@
+/* Question: print the pattern
+* 
+* * 
+* * * 
+* * * * 
+* * *     (for n number of stars or wanted by user in this same pattern)
+* * 
+* 
+  */
+import java.util.Scanner;                  
+public class PatternPrinting3 {
+  public static void main(String[] args){
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter a number:")
+   int number=sc.nextInt();
+   
+  for(int i=1;i<=number;i++){
+    for(int j=1;j<=i;j++){
+  System.out.print("* ");
+    }
+    System.out.println( );
+  }
+    for(int i=1;i<=number;i++){
+    for(int j=number-1;j>=i;j--){
+  System.out.print("* ");
+    }
+    System.out.println( );
+  }
+}
+}
