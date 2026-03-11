728694ffdddabb37c14d92cf92e416cafeac3036
Anon-691 <a.adeel2027@gmail.com>
2026-03-11 22:33:54 +0500

    Update PatternPrinting2.java

--- PatternPrinting2.java
+++ PatternPrinting2.java
@@ -2,13 +2,17 @@
 * 
 * * 
 * * * 
-* * * * 
-* * * * *
+* * * *  
+* * * * * (for n number of stars or wanted by user in this same pattern)
   */
-                   
+import java.util.Scanner;                  
 public class PatternPrinting2 {
   public static void main(String[] args){
-  for(int i=1;i<=5;i++){
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter a number:");
+   int number=sc.nextInt();
+   
+  for(int i=1;i<=number;i++){
     for(int j=1;j<=i;j++){
   System.out.print("* ");
     }
