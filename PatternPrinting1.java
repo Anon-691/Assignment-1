ee0a762ab5fe9b05741f38d63220b933878ffd6a
Anon-691 <a.adeel2027@gmail.com>
2026-03-11 22:02:15 +0500

    Update and rename PatternPrinting.java to PatternPrinting1.java

--- PatternPrinting1.java
+++ PatternPrinting1.java
@@ -1,5 +1,5 @@
 //Question : Print * * * * *
-public class PatternPrinting {
+public class PatternPrinting1 {
   public static void main(String[] args){
   for(int i=1;i<=5;i++){
   System.out.print("* ");
