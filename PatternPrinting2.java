855d4125960444b6e08b9dbae89ad583560fb780
Anon-691 <a.adeel2027@gmail.com>
2026-03-11 22:17:49 +0500

    Create PatternPrinting2.java

---
+++ PatternPrinting2.java
@@ -0,0 +1,18 @@
+/*Question: print
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+  */
+                   
+public class PatternPrinting2 {
+  public static void main(String[] args){
+  for(int i=1;i<=5;i++){
+    for(int j=1;j<=i;j++){
+  System.out.print("* ");
+    }
+    System.out.println( );
+  }
+}
+}
