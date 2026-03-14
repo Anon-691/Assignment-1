112a6160950e3a1a280fc633c017fb0f40b9e716
Anon-691 <a.adeel2027@gmail.com>
2026-03-14 16:06:21 +0500

    Create PopulationCalculator.java

---
+++ PopulationCalculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class PopulationCalculator{
+public static void main(String[] args){
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter number of populations :");
+int n=sc.nextInt();
+int[] arr = new int[n];
+int i=0; 
+int totPop=0;
+
+System.out.println("Enter populations :");
+while(i<n){
+int num=sc.nextInt();
+
+if(num<0){
+ break;
+}
+arr[i]=num;
+i++;
+totPop+=num;
+}
+
+System.out.println("Your populations are:");
+for(int var:arr){
+    System.out.println(var);
+}
+
+System.out.println("Total population =" + totPop);
+double avg=(totPop)/i;
+
+if(i > 0){
+            System.out.println("Population Average = " + avg );
+            
+}
+  }
+}
+
+  }
+}
