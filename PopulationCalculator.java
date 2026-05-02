06ebff595d43a6694871ff7d94b7a66cf516375c
Anon-691 <a.adeel2027@gmail.com>
2026-05-02 10:57:04 +0500

    Update PopulationCalculator.java
    Refactored PopulationCalculator to streamline input handling and calculate average population.

--- PopulationCalculator.java
+++ PopulationCalculator.java
@@ -1,37 +1,33 @@
 import java.util.Scanner;
-public class PopulationCalculator{
-public static void main(String[] args){
-
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter number of populations :");
-int n=sc.nextInt();
-int[] arr = new int[n];
-int i=0; 
-int totPop=0;
-
-System.out.println("Enter populations :");
-while(true){
-int num=sc.nextInt();
-
-if(num<0){
- break;
-}
-arr[i]=num;
-i++;
-totPop+=num;
-}
-
-System.out.println("Your populations are:");
-for(int j=0;j<i;j++){
-    System.out.println(arr[j]);
-}
-
-System.out.println("Total population =" + totPop);
-double avg=(totPop)/i;
-
-if(i > 0){
-            System.out.println("Population Average = " + avg );
+public class PopulationCalculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter populations");
+        int pop=sc.nextInt();
+        int[] population=new int[300];
+        int tpop=0;
+        int count=0;
+        int i=0;
+     
+        while(pop>=0){
+        
+            population[i]=pop;
+            tpop+=pop;
+            i++;
+            count++;
             
-}
-  }
+            pop=sc.nextInt();
+        }
+         if (count > 0) {
+            double avgpop = 1.0 * tpop / count;
+            System.out.println("Populations are:");
+            for (int j = 0; j < i; j++) {
+                System.out.println(population[j]);
+            }
+
+            System.out.println("Average is:\n"+avgpop);
+        } else {
+            System.out.println("No valid input");
+        }
+    }
 }
